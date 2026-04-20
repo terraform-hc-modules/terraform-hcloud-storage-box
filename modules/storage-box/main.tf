5e0d17b4d55c2dbe399a74816a271e99e273c617
Anmol Nagpal <anmol@clouddrove.com>
2026-04-20 08:29:54 -0400

    fix: use access_settings and snapshot_plan as attributes

--- modules/storage-box/main.tf
+++ modules/storage-box/main.tf
@@ -8,28 +8,8 @@ resource "hcloud_storage_box" "this" {
   labels            = var.labels
   delete_protection = var.delete_protection
   ssh_keys          = var.ssh_keys
-
-  dynamic "access_settings" {
-    for_each = var.access_settings != null ? [var.access_settings] : []
-    content {
-      reachable_externally = access_settings.value.reachable_externally
-      samba_enabled        = access_settings.value.samba_enabled
-      ssh_enabled          = access_settings.value.ssh_enabled
-      webdav_enabled       = access_settings.value.webdav_enabled
-      zfs_enabled          = access_settings.value.zfs_enabled
-    }
-  }
-
-  dynamic "snapshot_plan" {
-    for_each = var.snapshot_plan != null ? [var.snapshot_plan] : []
-    content {
-      max_snapshots = snapshot_plan.value.max_snapshots
-      minute        = snapshot_plan.value.minute
-      hour          = snapshot_plan.value.hour
-      day_of_week   = snapshot_plan.value.day_of_week
-      day_of_month  = snapshot_plan.value.day_of_month
-    }
-  }
+  access_settings   = var.access_settings
+  snapshot_plan     = var.snapshot_plan
 
   lifecycle {
     ignore_changes = [ssh_keys]
